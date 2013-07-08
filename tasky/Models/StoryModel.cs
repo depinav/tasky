f4d7735d6812882c835ae80f91c93b2d887d68eb
unknown <elliottm@19409-E6420.2d.hq>
2013-07-08 14:50:34 -0500

    added base CRUD for stories. doesn't include foreign keys yet

---
+++ tasky/Models/StoryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace tasky.Models
+{
+    public class Story
+    {
+        public int ID { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        //creator
+        //assignee
+        public int points { get; set; }
+        //sprint
+        public string status { get; set; }
+    }
+
+    public class StoryDBContext : DbContext
+    {
+        public DbSet<Story> Stories { get; set; }
+    }
+}
