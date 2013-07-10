f1ab6eb0dff6007c8b56f38a2f33c8994bbf1f97
Victor DePina <depinav@gmail.com>
2013-07-10 11:11:30 -0500

    Modified tasks so that status is a dropdown

---
+++ tasky/Migrations/201307101559160_taskmodel.cs
@@ -0,0 +1,16 @@
+namespace tasky.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class taskmodel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
