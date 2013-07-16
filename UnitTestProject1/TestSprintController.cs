fc25996e9d1280cbce8d536c3d58805e32dbb4d5
Elliott Mahler <join.together@gmail.com>
2013-07-16 09:20:59 -0500

    trying to fix appharbor

---
+++ UnitTestProject1/TestSprintController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using tasky.Models;
+using tasky.DAL;
+using tasky.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tasky.Controllers;
+using System.Collections.Generic;
+using Moq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestSprintController
+    {
+        [TestMethod]
+        public void TestListAll()
+        {
+            // Arrange
+            var mockRepo = new Mock<ISprintRepository>();
+            var sprints = new[] { new Sprint { id = 1, title = "test" } };
+            mockRepo.Setup(cr => cr.FindAll()).Returns(sprints);
+            var controller = new SprintController(mockRepo.Object);
+
+            // Act
+            var result = (ViewResult)controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(IEnumerable<Sprint>));
+        }
+    }
+}
