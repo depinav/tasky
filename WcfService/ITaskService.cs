f4c43538b69c63574c0853171fba5b87478959cb
Victor DePina <depinav@gmail.com>
2013-07-19 09:31:12 -0500

    Started wcf stuff

---
+++ WcfService/ITaskService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using tasky.Models;
+
+namespace WcfService {
+
+    [ServiceContract]
+    public interface ITaskService {
+
+        [OperationContract]
+        ICollection<Task> getTasks();
+    }
+}
---
+++ WcfService/TaskService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using tasky.DAL;
+using tasky.Models;
+using tasky.Repository;
+
+namespace WcfService
+{
+    public class TaskService : ITaskService {
+
+        private ITaskRepository taskRepo;
+
+        TaskService() { }
+
+        TaskService(ITaskRepository iTask) {
+            taskRepo = iTask;
+        }
+
+        public ICollection<Task> getTasks() {
+
+            return taskRepo.FindAll();
+        }
+    }
+}
